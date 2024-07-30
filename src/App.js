fde6ad44ddac5661ecf0cde9b2af29c8d93bf2b6
Sai Divya <saidivya@Sais-MacBook-Pro-3.local>
2024-07-30 00:20:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary libraries
+import React, { useState} from "react";
+import "./App.css";
 
 function App() {
+  const [upd, setUpd] = useState({
+    x: 0,
+    y: 0,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img
+        src="https://cdn.pixabay.com/animation/2023/07/01/12/25/12-25-50-208_512.gif" alt="img"
+        style={{
+          width: 200,
+          height: 200,
+          transition: "all 1s",
+          position: "absolute",
+          left: upd.x,
+          top: upd.y,
+        }}
+        onMouseOver={() => {
+          setUpd({
+            x: (Math.random() * 7860) % window.innerWidth,
+            y: (Math.random() * 1000) % window.innerHeight,
+          });
+        }}
+      />
+    </>
   );
 }
-
 export default App;
